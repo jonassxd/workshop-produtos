dd676fb62aac1306364266180b44659772bc0304
Jonas Eduardo <jondudys@gmail.com>
2023-02-09 14:54:44 -0300

    H2 database, test profile, JPA

--- src/main/java/educaweb/com/course/usuario/Usuario.java
+++ src/main/java/educaweb/com/course/usuario/Usuario.java
@@ -2,9 +2,19 @@ package educaweb.com.course.usuario;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;

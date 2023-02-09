d34e5afbda7e47a6103ce7a6f395d57860c48777
Jonas Eduardo <jondudys@gmail.com>
2023-02-09 15:15:48 -0300

    Repositório JPA, injeção de depedência, propagação de banco de dados

---
+++ src/main/java/educaweb/com/course/config/TesteConfig.java
@@ -0,0 +1,30 @@
+package educaweb.com.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import educaweb.com.course.repositores.UsuarioRepositor;
+import educaweb.com.course.usuario.Usuario;
+
+@Configuration
+@Profile("test")
+public class TesteConfig implements CommandLineRunner {
+	
+	@Autowired
+	private UsuarioRepositor usuarioRepositor;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		usuarioRepositor.saveAll(Arrays.asList(u1, u2));
+		
+	}
+	
+	
+}
---
+++ src/main/java/educaweb/com/course/repositores/UsuarioRepositor.java
@@ -0,0 +1,9 @@
+package educaweb.com.course.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import educaweb.com.course.usuario.Usuario;
+
+public interface UsuarioRepositor extends JpaRepository<Usuario, Long> {
+
+}

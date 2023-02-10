400d2616a9dc30d076b46beb0a439e73d550fa88
Jonas Eduardo <jondudys@gmail.com>
2023-02-10 13:59:35 -0300

    Pedido de item, associação muitos-para-muitos com atributos extras

---
+++ src/main/java/educaweb/com/course/entidade/ItemPedido.java
@@ -0,0 +1,85 @@
+package educaweb.com.course.entidade;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import educaweb.com.course.entidade.pk.ItemPedidoPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido_item")
+public class ItemPedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemPedidoPK id;
+
+	private Integer quantidade;
+	private Double preco;
+
+	public ItemPedido() {
+
+	}
+
+	public ItemPedido(Pedido pedido, Produto prroduto, Integer quantidade, Double price) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(prroduto);
+		this.quantidade = quantidade;
+		this.preco = price;
+
+	}
+
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+
+	public void SetPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPrice() {
+		return preco;
+	}
+
+	public void setPrice(Double price) {
+		this.preco = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}

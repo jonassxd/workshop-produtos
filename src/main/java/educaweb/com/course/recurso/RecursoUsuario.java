dd004d7e88d6de919375851f59314be79b7e71bd
Jonas Eduardo <jondudys@gmail.com>
2023-02-13 17:30:17 -0300

    Atualização de Usuário

--- src/main/java/educaweb/com/course/recurso/RecursoUsuario.java
+++ src/main/java/educaweb/com/course/recurso/RecursoUsuario.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,39 +17,42 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import educaweb.com.course.entidade.Usuario;
 import educaweb.com.course.service.UsuarioServico;
-import jakarta.servlet.Servlet;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class RecursoUsuario {
-	
+
 	@Autowired
 	private UsuarioServico servico;
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<Usuario>>findAll(){
+	public ResponseEntity<List<Usuario>> findAll() {
 		List<Usuario> list = servico.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
 		Usuario obj = servico.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
 	@PostMapping
-	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj){
+	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj) {
 		obj = servico.insert(obj);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();		
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		servico.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody Usuario obj){
+		obj = servico.atualizar(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/educaweb/com/course/service/UsuarioServico.java
+++ src/main/java/educaweb/com/course/service/UsuarioServico.java
@@ -4,32 +4,50 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import educaweb.com.course.entidade.Usuario;
 import educaweb.com.course.repositores.UsuarioRepositor;
 
 @Service
 public class UsuarioServico {
-	
+
 	@Autowired
 	private UsuarioRepositor repositor;
-	
-	public List<Usuario> findAll(){
-		
-		
+
+	public List<Usuario> findAll() {
+
 		return repositor.findAll();
-		
+
 	}
+
 	public Usuario findById(Long id) {
-	Optional<Usuario> obj = repositor.findById(id);
-	return obj.get();
-	
+		Optional<Usuario> obj = repositor.findById(id);
+		return obj.get();
+
 	}
+
 	public Usuario insert(Usuario obj) {
 		return repositor.save(obj);
 	}
+
 	public void delete(Long id) {
 		repositor.deleteById(id);
+	}
+
+	public Usuario atualizar(Long id, Usuario obj) {
+		Usuario entidade = repositor.getReferenceById(id);
+		atualizarDados(entidade, obj);
+		return repositor.save(entidade);
+	}
+
+	private void atualizarDados(Usuario entidade, Usuario obj) {
+		entidade.setNome(obj.getNome());
+		entidade.setEmail(obj.getEmail());
+		entidade.setTelefone(obj.getTelefone());
 	}
 }
